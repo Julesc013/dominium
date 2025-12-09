3b77d6cab889442c76b0b72d88b3e8314408bcb8
Jules Carboni <30209022+Julesc013@users.noreply.github.com>
2025-12-10 00:40:42 +1100

    setup 3

---
+++ source/dominium/products/setup/os/carbon/setup_carbon.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+    printf("Dominium Carbon installer stub.\n");
+    printf("TODO: implement Carbon OS TUI to copy binaries/data and register in launcher.\n");
+    return 0;
+}
---
+++ source/dominium/products/setup/os/win16/setup_win16_tui.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+    printf("Dominium Win16 installer stub.\n");
+    printf("TODO: implement Win16 TUI/dialog installer (Install/Repair/Uninstall).\n");
+    return 0;
+}
