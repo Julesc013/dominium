dc9d313a8494606ea002dba1957ec6237a24f1f5
Jules Carbon <30209022+Julesc013@users.noreply.github.com>
2026-03-03 07:21:19 +1100

    SIG6-5: Standards body and certification workflow integrated

--- src/signals/__init__.py
+++ src/signals/__init__.py
@@ -78,16 +78,22 @@ from .trust import (
 )
 from .institutions import (
     REFUSAL_DISPATCH_POLICY_FORBIDDEN,
+    REFUSAL_STANDARDS_SPEC_TYPE_FORBIDDEN,
     build_dispatch_update,
     build_institution_profile,
+    build_spec_issue_request,
     bulletin_policy_rows_by_id,
     deterministic_dispatch_update_id,
+    deterministic_spec_issue_id,
     dispatch_policy_rows_by_id,
     institution_profile_rows_by_id,
     normalize_dispatch_update_rows,
     normalize_institution_profile_rows,
+    normalize_spec_issue_request_rows,
     process_institution_bulletin_tick,
     process_dispatch_issue_updates,
+    process_standards_issue_and_report,
+    standards_policy_rows_by_id,
 )
 
 __all__ = [
@@ -160,14 +166,20 @@ __all__ = [
     "trust_update_rule_rows_by_id",
     "trust_weight_for_pair",
     "REFUSAL_DISPATCH_POLICY_FORBIDDEN",
+    "REFUSAL_STANDARDS_SPEC_TYPE_FORBIDDEN",
     "build_dispatch_update",
     "build_institution_profile",
+    "build_spec_issue_request",
     "bulletin_policy_rows_by_id",
     "deterministic_dispatch_update_id",
+    "deterministic_spec_issue_id",
     "dispatch_policy_rows_by_id",
     "institution_profile_rows_by_id",
     "normalize_dispatch_update_rows",
     "normalize_institution_profile_rows",
+    "normalize_spec_issue_request_rows",
     "process_institution_bulletin_tick",
     "process_dispatch_issue_updates",
+    "process_standards_issue_and_report",
+    "standards_policy_rows_by_id",
 ]
--- src/signals/institutions/__init__.py
+++ src/signals/institutions/__init__.py
@@ -15,17 +15,31 @@ from .dispatch_engine import (
     normalize_dispatch_update_rows,
     process_dispatch_issue_updates,
 )
+from .standards_engine import (
+    REFUSAL_STANDARDS_SPEC_TYPE_FORBIDDEN,
+    build_spec_issue_request,
+    deterministic_spec_issue_id,
+    normalize_spec_issue_request_rows,
+    process_standards_issue_and_report,
+    standards_policy_rows_by_id,
+)
 
 __all__ = [
     "REFUSAL_DISPATCH_POLICY_FORBIDDEN",
+    "REFUSAL_STANDARDS_SPEC_TYPE_FORBIDDEN",
     "build_dispatch_update",
     "build_institution_profile",
+    "build_spec_issue_request",
     "bulletin_policy_rows_by_id",
     "deterministic_dispatch_update_id",
+    "deterministic_spec_issue_id",
     "dispatch_policy_rows_by_id",
     "institution_profile_rows_by_id",
     "normalize_dispatch_update_rows",
     "normalize_institution_profile_rows",
+    "normalize_spec_issue_request_rows",
     "process_institution_bulletin_tick",
     "process_dispatch_issue_updates",
+    "process_standards_issue_and_report",
+    "standards_policy_rows_by_id",
 ]
