f8328e49a2ad0f605b477528beb1779f0aa15d1c
Jules Carbon <30209022+Julesc013@users.noreply.github.com>
2026-03-06 21:44:10 +1100

    PROC5-4: Deterministic process capsule execution implemented

--- src/process/capsules/__init__.py
+++ src/process/capsules/__init__.py
@@ -11,6 +11,13 @@ from src.process.capsules.capsule_builder import (
     normalize_process_capsule_rows,
     process_capsule_rows_by_id,
 )
+from src.process.capsules.capsule_executor import (
+    REFUSAL_PROCESS_CAPSULE_EXECUTION_REFUSED,
+    REFUSAL_PROCESS_CAPSULE_OUT_OF_DOMAIN,
+    build_capsule_execution_record_row,
+    execute_process_capsule,
+    normalize_capsule_execution_record_rows,
+)
 
 __all__ = [
     "REFUSAL_PROCESS_CAPSULE_INVALID",
@@ -22,4 +29,9 @@ __all__ = [
     "process_capsule_rows_by_id",
     "build_capsule_generated_record_row",
     "generate_process_capsule",
+    "REFUSAL_PROCESS_CAPSULE_OUT_OF_DOMAIN",
+    "REFUSAL_PROCESS_CAPSULE_EXECUTION_REFUSED",
+    "build_capsule_execution_record_row",
+    "normalize_capsule_execution_record_rows",
+    "execute_process_capsule",
 ]
